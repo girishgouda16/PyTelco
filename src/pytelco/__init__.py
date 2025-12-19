0b86e0e6590e40cef6245948ecb0dde66eea0411
Girish Kumar Gouda <girishgouda6@gmail.com>
2025-12-19 11:31:06 +0000

    feat: Bump project version and expand gitignore rules for data and docs directories.

--- src/pytelco/__init__.py
+++ src/pytelco/__init__.py
@@ -10,7 +10,7 @@ Modules:
     io            - Data loaders for SIP, GTP-U, CDR
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 # Core utilities (most used)
 from .preprocessing.time_series import (
